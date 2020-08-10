8fa411918849a5794153d7f8538a0146639ac965
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2020-08-10 13:30:48 -0500

    completed tier 1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		long sum = 0;
+		long tmpNum = num;
+		//casting to int floors the decimal
+		final int len = 1 + (int)Math.log10(tmpNum);
+		while (tmpNum > 0) {
+			sum += Math.pow(tmpNum%10, len);
+			tmpNum /= 10;
+		}
+
+		return sum == num;
 	}
 }
